18ae7e5cdaac733e7e2a5e2dd1f746ffdafe498e
Johnnie Walker <rockwave.fm@gmail.com>
2019-10-11 12:48:33 +0300

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Johnnie Walker on 11.10.2019.
+//  Copyright © 2019 Johnnie Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
